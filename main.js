25d925909d51ebbeeca46da8b686497f25ffd043
Niko Shin <nshin17@gmail.com>
2022-10-19 14:42:46 -0700

    Minor updates

--- main.js
+++ main.js
@@ -1,33 +1,40 @@
 // Todo: Fix carousel dot issue not connecting properly during image sliding.
 
-// Respective carousel dots are selected based on the image presented:
+// Loops over all li element to check if [data-active] attribute is valid => if it is, get the id of that list and corresponding input element should also be checked:
+// Get list element id that has the [data-active] attribute on:
 
-function carouselDotsSelector() {
-  // let selectedImg;
+function getListElementId() {
+  const liAll = document.querySelectorAll("li");
+  let result;
 
-  const li = document.querySelectorAll("li");
-
-  li.forEach((element) => {
-    if (element.hasAttribute("data-active")) {
-      switch (element.id) {
-        case "img1":
-          document.querySelector("#radio1").setAttribute("checked", "checked");
-          break;
-        case "img2":
-          document.querySelector("#radio2").setAttribute("checked", "checked");
-          break;
-        case "img3":
-          document.querySelector("#radio3").setAttribute("checked", "checked");
-          break;
-        case "img4":
-          document.querySelector("#radio4").setAttribute("checked", "checked");
-          break;
-        default:
-
-        // nothing.
-      }
+  liAll.forEach((item) => {
+    if (item.hasAttribute("data-active")) {
+      result = item.id;
     }
   });
+  return result;
+}
+
+// Use the getListElementId() to determine which input element will have the 'checked' attribute:
+
+function addCheckAtt() {
+  switch (getListElementId()) {
+    case "img1":
+      document.querySelector("#radio1").setAttribute("checked", true);
+      break;
+    case "img2":
+      document.querySelector("#radio2").setAttribute("checked", true);
+      break;
+    case "img3":
+      document.querySelector("#radio3").setAttribute("checked", true);
+      break;
+    case "img4":
+      document.querySelector("#radio4").setAttribute("checked", true);
+      break;
+    default:
+
+    // nothing.
+  }
 }
 
 // Check for existing 'checked' attribute in all input elements and remove them if found:
@@ -42,76 +49,66 @@ function removeCheckedAtt() {
   });
 }
 
-function arrowBtn() {
-  const buttons = document.querySelectorAll("[data-carousel-button");
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-      const slides = button
-        .closest("[data-carousel]")
-        .querySelector("[data-slides");
+const buttons = document.querySelectorAll("[data-carousel-button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    removeCheckedAtt();
 
-      const activeSlide = slides.querySelector("[data-active]");
-      let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-      if (newIndex < 0) {
-        newIndex = slides.children.length - 1;
-      }
-      if (newIndex >= slides.children.length) {
-        newIndex = 0;
-      }
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides");
 
-      slides.children[newIndex].dataset.active = true;
-      delete activeSlide.dataset.active;
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) {
+      newIndex = slides.children.length - 1;
+    }
+    if (newIndex >= slides.children.length) {
+      newIndex = 0;
+    }
 
-      removeCheckedAtt();
-      carouselDotsSelector();
-    });
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+    addCheckAtt();
   });
-}
+});
 
 // If user selects a specific radio button id, that image should have the active class on it:
 
-function removeAtt(data) {
+function removeAtt() {
   const li = document.querySelectorAll("li");
 
   li.forEach((att) => {
-    if (att.hasAttribute(data)) {
-      att.removeAttribute(data);
+    if (att.hasAttribute("data-active")) {
+      att.removeAttribute("data-active");
     }
   });
 }
 
-function addDataActiveAtt() {
-  const carouselDots = document.querySelectorAll("[data-carousel-dots]");
-  carouselDots.forEach((dot) => {
-    dot.addEventListener("click", (e) => {
-      removeAtt("data-active"); // Remove [data-active] attribute at the beginning.
-      switch (e.target.id) {
-        case "radio1":
-          document.querySelector("#img1").setAttribute("data-active", "");
-          break;
-        case "radio2":
-          document.querySelector("#img2").setAttribute("data-active", "");
-          break;
-        case "radio3":
-          document.querySelector("#img3").setAttribute("data-active", "");
-          break;
-        case "radio4":
-          document.querySelector("#img4").setAttribute("data-active", "");
-          break;
-        default:
+const carouselDots = document.querySelectorAll("[data-carousel-dots]");
+carouselDots.forEach((dot) => {
+  dot.addEventListener("click", (e) => {
+    removeAtt(); // Remove [data-active] attribute at the beginning.
+    removeCheckedAtt();
 
-        // do nothing.
-      }
-      carouselDotsSelector();
-    });
+    switch (e.target.id) {
+      case "radio1":
+        document.querySelector("#img1").setAttribute("data-active", true);
+        break;
+      case "radio2":
+        document.querySelector("#img2").setAttribute("data-active", true);
+        break;
+      case "radio3":
+        document.querySelector("#img3").setAttribute("data-active", true);
+        break;
+      case "radio4":
+        document.querySelector("#img4").setAttribute("data-active", true);
+        break;
+      default:
+
+      // do nothing.
+    }
+    addCheckAtt();
   });
-}
-
-function render() {
-  arrowBtn();
-  addDataActiveAtt();
-  carouselDotsSelector();
-}
-
-render();
+});
